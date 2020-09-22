c061f128f8dcd569e3f1edce8c60054f3021ea73
meltemmbilir <meltemmbilir@gmail.com>
2020-09-22 21:49:50 +0300

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,5 +25,8 @@ namespace CourseApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public void Deneme(){
+          string deneme = string.Empty;
+        }
     }
 }
